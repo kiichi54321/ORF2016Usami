50dd9ff2d2ef19ead4b22ee0bc257855dfec16ce
kiich <kiich@DESKTOP-L6K4JDB>
2016-11-07 21:03:15 +0900

    プロジェクト ファイルを追加します。

---
+++ UsamiWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UsamiWebApplication.MvcApplication" Language="C#" %>
---
+++ UsamiWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UsamiWebApplication.Startup))]
+namespace UsamiWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
